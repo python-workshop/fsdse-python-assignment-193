e843108c602d32268e32710b7c0335ab5fbd542f
sangam <sangam.angre@gmail.com>
2017-05-23 11:57:17 +0530

    test cases created

--- build.py
+++
@@ -1,8 +0,0 @@
-def sum_two(a, b):
-    if a is None or b is None:
-        return False
-    result = a ^ b;
-    carry = (a & b) << 1
-    if carry != 0:
-        return sum_two(result, carry)
-    return result;
